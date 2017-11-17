f2e2ebec1abe1c6f0a39e9e5b5e9ae641415d6ae
Stackmann <ole.yaroshenko@gmail.com>
2017-11-17 22:26:27 +0200

    Initial Commit

---
+++ MySecrets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySecrets
+//
+//  Created by Eric on 11/17/17.
+//  Copyright © 2017 Piligrim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
