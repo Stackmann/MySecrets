859d4ca26cad0e17b153dd38f5ae1bfb8cbb8776
Stackmann <ole.yaroshenko@gmail.com>
2017-12-01 00:26:49 +0200

    add collectionview

---
+++ MySecrets/PwdCollectionViewController.swift
@@ -0,0 +1,92 @@
+//
+//  PwdCollectionViewController.swift
+//  MySecrets
+//
+//  Created by Eric on 11/30/17.
+//  Copyright © 2017 Piligrim. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class PwdCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if Secrets.share.list == nil {
+            performSegue(withIdentifier: "enterPwd", sender: nil)
+        } else {
+            self.collectionView!.reloadData()
+        }
+
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return Secrets.share.list?.count ?? 0
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "secretCell", for: indexPath) as! SecretCell
+        if let arraySecrets = Secrets.share.list, arraySecrets.indices.contains(indexPath.row){
+            // Configure the cell
+            let secret = arraySecrets[indexPath.row]
+           cell.configure(image: secret.avatar, descr: secret.describe)
+        }
+
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
---
+++ MySecrets/SecretCell.swift
@@ -0,0 +1,27 @@
+//
+//  SecretCell.swift
+//  MySecrets
+//
+//  Created by Eric on 11/30/17.
+//  Copyright © 2017 Piligrim. All rights reserved.
+//
+
+import UIKit
+
+class SecretCell: UICollectionViewCell {
+    
+    @IBOutlet weak var secretDescription: UILabel!
+   
+    @IBOutlet weak var secretIcon: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        secretDescription.numberOfLines = 0
+    }
+
+    func configure(image: UIImage, descr: String) {
+        secretDescription.text = descr
+        secretIcon.image = image
+    }
+
+}
