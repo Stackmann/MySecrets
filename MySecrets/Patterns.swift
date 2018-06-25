fc07885770e2811263d2ce12a93eab25049931f9
Stackmann <ole.yaroshenko@gmail.com>
2018-06-25 20:57:25 +0300

    add icons for patterns, increase size avatar image view on credicardEditViewController

--- MySecrets/Patterns.swift
+++ MySecrets/Patterns.swift
@@ -15,51 +15,61 @@ class Patterns {
     
     init() {
         var listOfPatterns = [String: PatternRecord]()
-        guard let avatarCreditCard = UIImage(named: "credit_card_pattern") else { list = listOfPatterns; return }
-        guard let avatar2 = UIImage(named: "if_credit-cards_47679") else { list = listOfPatterns; return }
-        guard let avatar3 = UIImage(named: "if_lock_49374") else { list = listOfPatterns; return }
-        guard let avatar4 = UIImage(named: "if_secure-payment_47685") else { list = listOfPatterns; return }
-        guard let avatarIdCard = UIImage(named: "user_id_pattern") else { list = listOfPatterns; return }
+        guard let avatarCreditCard = UIImage(named: "credit-cards64x64") else { list = listOfPatterns; return }
+        guard let avatarWWW = UIImage(named: "world64x64") else { list = listOfPatterns; return }
+        guard let avatarEmail = UIImage(named: "email64x64") else { list = listOfPatterns; return }
+        guard let avatarWiFi = UIImage(named: "wireless64x64") else { list = listOfPatterns; return }
+        guard let avatarPassword = UIImage(named: "key64") else { list = listOfPatterns; return }
+        guard let avatarRDP = UIImage(named: "rdp64x64") else { list = listOfPatterns; return }
+        guard let avatarAccount = UIImage(named: "user64x64") else { list = listOfPatterns; return }
+        guard let avatar3 = UIImage(named: "bank64") else { list = listOfPatterns; return }
+        guard let avatarBonusCard = UIImage(named: "shopping-basket64x64") else { list = listOfPatterns; return }
+        guard let avatarIdCard = UIImage(named: "idcard64x64") else { list = listOfPatterns; return }
 
         guard let avatarCreditCardData = UIImagePNGRepresentation(avatarCreditCard) else { list = listOfPatterns; return }
-        guard let avatarData2 = UIImagePNGRepresentation(avatar2) else { list = listOfPatterns; return }
+        guard let avatarWWWData = UIImagePNGRepresentation(avatarWWW) else { list = listOfPatterns; return }
+        guard let avatarEmailData = UIImagePNGRepresentation(avatarEmail) else { list = listOfPatterns; return }
+        guard let avatarWiFiData = UIImagePNGRepresentation(avatarWiFi) else { list = listOfPatterns; return }
+        guard let avatarPasswordData = UIImagePNGRepresentation(avatarPassword) else { list = listOfPatterns; return }
+        guard let avatarRDPData = UIImagePNGRepresentation(avatarRDP) else { list = listOfPatterns; return }
+        guard let avatarAccountData = UIImagePNGRepresentation(avatarAccount) else { list = listOfPatterns; return }
         guard let avatarData3 = UIImagePNGRepresentation(avatar3) else { list = listOfPatterns; return }
-        guard let avatarData4 = UIImagePNGRepresentation(avatar4) else { list = listOfPatterns; return }
+        guard let avatarBonusCardData = UIImagePNGRepresentation(avatarBonusCard) else { list = listOfPatterns; return }
         guard let avatarIdCardData = UIImagePNGRepresentation(avatarIdCard) else { list = listOfPatterns; return }
 
         var patternFields = ["Login": "String",
                              "Password": "String",
                              "URL": "String",
                              "Notes": "String"]
-        var newPattern = PatternRecord(describe: "Websait", fields: patternFields, avatar: avatarData2, kind: PatternKind.websait)
+        var newPattern = PatternRecord(describe: "Websait", fields: patternFields, avatar: avatarWWWData, kind: PatternKind.websait)
         listOfPatterns[PatternKind.websait.rawValue] = newPattern
 
         patternFields = ["Adress": "String",
                          "Login": "String",
                          "Password": "String"]
-        newPattern = PatternRecord(describe: "RDP Connection", fields: patternFields, avatar: avatarData3, kind: PatternKind.rdp)
+        newPattern = PatternRecord(describe: "RDP", fields: patternFields, avatar: avatarRDPData, kind: PatternKind.rdp)
         listOfPatterns[PatternKind.rdp.rawValue] = newPattern
 
         patternFields = ["Login": "String",
                          "Password": "String",
                          "Notes": "String"]
-        newPattern = PatternRecord(describe: "Account", fields: patternFields, avatar: avatarData4, kind: PatternKind.account)
+        newPattern = PatternRecord(describe: "Account", fields: patternFields, avatar: avatarAccountData, kind: PatternKind.account)
         listOfPatterns[PatternKind.account.rawValue] = newPattern
 
         patternFields = ["Password": "String",
                          "Notes": "String"]
-        newPattern = PatternRecord(describe: "Password", fields: patternFields, avatar: avatarData3, kind: PatternKind.password)
+        newPattern = PatternRecord(describe: "Password", fields: patternFields, avatar: avatarPasswordData, kind: PatternKind.password)
         listOfPatterns[PatternKind.password.rawValue] = newPattern
 
         patternFields = ["Number": "Int",
                          "Notes": "String"]
-        newPattern = PatternRecord(describe: "Bonus card", fields: patternFields, avatar: avatarData4, kind: PatternKind.bonuscard)
+        newPattern = PatternRecord(describe: "Bonus card", fields: patternFields, avatar: avatarBonusCardData, kind: PatternKind.bonuscard)
         listOfPatterns[PatternKind.bonuscard.rawValue] = newPattern
 
         patternFields = ["SSID": "String",
                          "Password": "String",
                          "Notes": "String"]
-        newPattern = PatternRecord(describe: "WiFi", fields: patternFields, avatar: avatarData2, kind: PatternKind.wifi)
+        newPattern = PatternRecord(describe: "WiFi", fields: patternFields, avatar: avatarWiFiData, kind: PatternKind.wifi)
         listOfPatterns[PatternKind.wifi.rawValue] = newPattern
 
         patternFields = ["Login": "String",
@@ -68,7 +78,7 @@ class Patterns {
                          "IMAP": "String",
                          "SMTP": "String",
                          "Notes": "String"]
-        newPattern = PatternRecord(describe: "Email", fields: patternFields, avatar: avatarData3, kind: PatternKind.email)
+        newPattern = PatternRecord(describe: "Email", fields: patternFields, avatar: avatarEmailData, kind: PatternKind.email)
         listOfPatterns[PatternKind.email.rawValue] = newPattern
 
         patternFields = ["Bank": "String",
@@ -85,7 +95,7 @@ class Patterns {
                          "BankMFO": "Int",
                          "Account": "Int",
                          "Notes": "String"]
-        newPattern = PatternRecord(describe: "Bank account", fields: patternFields, avatar: avatarData3, kind: PatternKind.bankaccount)
+        newPattern = PatternRecord(describe: "Bank acc.", fields: patternFields, avatar: avatarData3, kind: PatternKind.bankaccount)
         listOfPatterns[PatternKind.bankaccount.rawValue] = newPattern
 
         patternFields = ["FirstName": "String",
