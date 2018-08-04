65b3abaaaeac10d6f09f43662efc4c9a4c787799
Stackmann <o.yaroshenko@gmail.com>
2018-08-05 00:16:05 +0300

    add view values of record in CommonViewController

--- MySecrets/CommonViewController.swift
+++ MySecrets/CommonViewController.swift
@@ -23,6 +23,7 @@ class CommonViewController: UIViewController {
     @IBOutlet weak var label4Value: UILabel!
     @IBOutlet weak var label5: UILabel!
     @IBOutlet weak var label5Value: UILabel!
+    @IBOutlet weak var patternImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,89 +41,33 @@ class CommonViewController: UIViewController {
             
             descriptionValue.text = chosenRecord.describe
             descriptionValue.textColor = UIColor.white
-            configureLabels(chosenRecord.idPattern)
             
-//            if let firstNameStr = chosenRecord.stringFields["FirstName"] {
-//                firstNameValue.textAlignment = NSTextAlignment.left
-//                firstNameValue.text = firstNameStr
-//                //firstNameValue = UIFont(name: "OCRAStd", size: 35)
-//                firstNameValue.textColor = UIColor.yellow
-//            } else {
-//                firstNameValue.text = ""
-//            }
-//            if let lastNameStr = chosenRecord.stringFields["LastName"] {
-//                lastNameValue.textAlignment = NSTextAlignment.left
-//                lastNameValue.text = lastNameStr
-//                //lastNameValue = UIFont(name: "OCRAStd", size: 35)
-//                lastNameValue.textColor = UIColor.yellow
-//            } else {
-//                lastNameValue.text = ""
-//            }
-//            if let middleNameStr = chosenRecord.stringFields["MiddleName"] {
-//                middleNameValue.textAlignment = NSTextAlignment.left
-//                middleNameValue.text = middleNameStr
-//                //middleNameValue = UIFont(name: "OCRAStd", size: 35)
-//                middleNameValue.textColor = UIColor.yellow
-//            } else {
-//                middleNameValue.text = ""
-//            }
-//
-//            if let BirthdayDateStr = chosenRecord.dateFields["BirthdayDate"] {
-//                let formatter = DateFormatter()
-//                formatter.dateFormat = "dd/MM/yyyy"
-//                birthdayValue.textAlignment = NSTextAlignment.left
-//                birthdayValue.text = formatter.string(from: BirthdayDateStr)
-//                //birthdayValue.font = UIFont(name: "OCRAStd", size: 20)
-//                birthdayValue.textColor = UIColor.yellow
-//            } else {
-//                birthdayValue.text = ""
-//            }
-//
-//            if let ReceivedDateStr = chosenRecord.dateFields["ReceivedDate"] {
-//                let formatter = DateFormatter()
-//                formatter.dateFormat = "dd/MM/yyyy"
-//                receivedDateValue.textAlignment = NSTextAlignment.left
-//                receivedDateValue.text = formatter.string(from: ReceivedDateStr)
-//                //receivedDateValue = UIFont(name: "OCRAStd", size: 20)
-//                receivedDateValue.textColor = UIColor.yellow
-//            } else {
-//                receivedDateValue.text = ""
-//            }
-//
-//            if let snStr = chosenRecord.stringFields["SN"] {
-//                snValue.textAlignment = NSTextAlignment.left
-//                snValue.text = snStr
-//                //snValue.font = UIFont(name: "OCRAStd", size: 20)
-//                snValue.textColor = UIColor.yellow
-//            } else {
-//                snValue.text = ""
-//            }
-//
-//            if let numberStr = chosenRecord.decimalFields["Number"] {
-//                numberValue.textAlignment = NSTextAlignment.left
-//                numberValue.text = "\(numberStr)"
-//                //numberValue.font = UIFont(name: "OCRAStd", size: 20)
-//                numberValue.textColor = UIColor.yellow
-//            } else {
-//                numberValue.text = ""
-//            }
+            if let patern = Patterns.share.list[chosenRecord.idPattern] {
+                let labels = [label1, label2, label3, label4, label5, label1Value, label2Value, label3Value, label4Value, label5Value]
+                var count = 0
+                for patternField in patern.fields {
+                    labels[count]?.text = patternField.key
+                    labels[count]?.textColor = UIColor.white
+                    if patternField.value == "String", let valueStr = chosenRecord.stringFields[patternField.key] {
+                        labels[count + maxRowOfLabelCount]?.textAlignment = NSTextAlignment.left
+                        labels[count + maxRowOfLabelCount]?.text = valueStr
+                        labels[count + maxRowOfLabelCount]?.textColor = UIColor.yellow
+                    } else {
+                        labels[count + maxRowOfLabelCount]?.text = ""
+                    }
+                    count += 1
+                }
+                while count < maxRowOfLabelCount {
+                    labels[count]?.isHidden = true
+                    labels[count + maxRowOfLabelCount]?.isHidden = true
+                    count += 1
+                }
+                if let image = UIImage(data: patern.avatar) {
+                    patternImageView.image = image
+                }
+            }
+
         }
     }
     
-    func configureLabels(_ id:String) {
-        if let patern = Patterns.share.list[id] {
-            let labels = [label1, label2, label3, label4, label5, label1Value, label2Value, label3Value, label4Value, label5Value]
-            var count = 0
-            for patternField in patern.fields {
-                labels[count]?.text = patternField.key
-                labels[count]?.textColor = UIColor.white
-                count += 1
-            }
-            while count < maxRowOfLabelCount {
-                labels[count]?.isHidden = true
-                labels[count + maxRowOfLabelCount]?.isHidden = true
-                count += 1
-            }
-        }
-    }
 }
